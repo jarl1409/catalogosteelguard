748a87c876da98cb6cb76e9979143d221b7346cd
jarl1409 <jhosepandrey203@gmail.com>
2025-11-26 14:42:02 -0500

    Fix: Arreglo de rutas imagen Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export const Header = ({ searchQuery, onSearchChange }) => {
         <div className="flex flex-col items-center lg:flex-row lg:justify-start lg:gap-8">
           <img
             className="w-48 lg:w-96 mb-4 lg:mb-0"
-            src="../assets/LOGOTIPO AMARILLO.png"
+            src="/src/assets/LOGOTIPO AMARILLO.png"
             alt="Logotipo Steelguard Amarillo"
           />
 
